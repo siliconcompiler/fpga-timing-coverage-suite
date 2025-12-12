a64eeea29b3a61d0a8e70bad2e1f846982c5aee4
Minchan Kwon <michael8395@korea.ac.kr>
2025-12-12 15:43:41 -0500

    [Suite] Added New RTL for Syntax Suite

---
+++ auto_generated/netlist_files/get_clocks_netlist.v
@@ -0,0 +1,26 @@
+/*
+Command: get_clocks
+Description:
+    -Returns a list of clocks matching patterns
+    
+SDC Example:
+    get_clocks clk1
+    get_clocks -quiet clk1098
+*/
+
+//Main module
+module get_clocks(
+    input wire clk1, 
+    input wire clk2,
+    input wire clk_gen,
+    input wire clock,
+    output wire dummy_out
+);
+    assign dummy_out = 1'b1;
+
+    always (posedge clk1 or posedge clk2 or posedge clk_gen or posedge clock) begin
+        dummy_out <= ~dummy_out;
+    end
+
+endmodule
+
---
+++ auto_generated/netlist_files/get_ports_netlist.v
@@ -0,0 +1,32 @@
+/*
+Command: get_ports
+Description:
+    -Returns a list of all top level ports that match patterns 
+    
+SDC Example:
+    get_ports data*
+    get_ports -regexp rst
+*/
+
+module get_ports(
+    input wire clk,
+    input wire rst,
+    input wire data1_in,
+    input wire data2_in,
+    input wire valid_in,
+    output reg result_out,
+    output reg ready_out
+);
+    //ready_out is always 1 for this simple module
+    assign ready_out = 1'b1;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            result_out <= 1'b0;
+        end
+        else if (valid_in) begin
+            result_out <= data1_in + data2_in;
+        end
+    end
+
+endmodule
